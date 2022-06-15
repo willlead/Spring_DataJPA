84b643a622295473d4644051485db1708aa1fb76
zinMonger <sumlead@gmail.com>
2022-06-15 17:08:11 +0900

    dto BeanUtils, restTemplate

--- Spring_DataJPA/src/main/java/com/example/demo/service/CustmerDtoServiceImpl.java
+++ Spring_DataJPA/src/main/java/com/example/demo/service/CustmerDtoServiceImpl.java
@@ -1,7 +1,12 @@
 package com.example.demo.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.demo.domain.entity.Customer;
 import com.example.demo.domain.entity.CustomerDto;
@@ -13,14 +18,59 @@ public class CustmerDtoServiceImpl implements CustomerDtoService {
 	@Autowired
 	CustomerRepository repository;
 	
+	@Autowired
+	RestTemplate restTemplate; // 동기
+	
+//	WebClient client; //동기, 비동기 모두 지원
+	//OpenFeign 
+ 	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
 	@Override
 	public int createCustomer(CustomerDto customerDto) {
-		Customer customer = Customer.builder().name(customerDto.getName()).address(customerDto.getAddress()).build();
+		//1. Dto -> Entity 변환
+//		Customer customer = Customer.builder().name(customerDto.getName()).address(customerDto.getAddress()).build();
+		
+		//2. Dto -> Entity 변환
+		Customer customer = Customer.builder().build();
+		BeanUtils.copyProperties(customerDto, customer);
+		
+		//3. Dto -> Entity 변환 라이브러: ModelMapper, MapStruct ...
+		
 		customer = repository.save(customer);
-		if(customer.getId() > 1) {
+		if(customer.getId() > 0) {
 			return 1;
 		}
 		return 0;
+	}
+	
+	public boolean isExistCustomer(Long id)
+	{
+		return repository.findById(id).isPresent();
+	}
+
+	@Override
+	public CustomerDto getCustomerById2(Long id) {
+		CustomerDto dto = CustomerDto.builder().build();
+		if(isExistCustomer(id)) {
+			Optional<Customer> optional = repository.findById(id);
+			BeanUtils.copyProperties(optional.get(), dto, "id");
+		}
+		return dto;
+	}
+
+	@Override
+	public CustomerDto getCustomerById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String callApi() {
+		return restTemplate.getForObject("http://localhost:8080/hi", String.class);
 	}
 
 }
--- Spring_DataJPA/src/main/java/com/example/demo/service/CustomerDtoService.java
+++ Spring_DataJPA/src/main/java/com/example/demo/service/CustomerDtoService.java
@@ -1,8 +1,12 @@
 package com.example.demo.service;
 
+import com.example.demo.domain.entity.Customer;
 import com.example.demo.domain.entity.CustomerDto;
 
 public interface CustomerDtoService {
 	public int createCustomer(CustomerDto customerDto);
+	public CustomerDto getCustomerById(Long id);
+	CustomerDto getCustomerById2(Long id);
+	public String callApi();
 
 }
